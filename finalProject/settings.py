d85c2e8c144dbae98ce8bcb3ef860b0ce269e1a0
KamranAkhenaton21 <62439616+KamranAkhenaton21@users.noreply.github.com>
2020-05-10 17:05:32 -0700

    HW

---
+++ finalProject/settings.py
@@ -0,0 +1,16 @@
+# © 2019 KidsCanCode LLC / All rights reserved.
+
+
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 255, 0)
+GREEN = (255, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 0, 0)
+
+TITLE = "Jumpy!"
+WIDTH = 480
+HEIGHT = 600
+FPS = 60
